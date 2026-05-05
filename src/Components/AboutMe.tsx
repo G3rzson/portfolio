af1c007d752138264ff06049eb64e86523cc28ae
G3rzson <gerzonpal@gmail.com>
2026-05-05 11:58:30 +0200

    fix: fixed ios render issue

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -16,8 +16,8 @@ export default function AboutMe() {
       />
 
       <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, y: 10 }}
+        whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.3 }}
         transition={{ duration: 1, ease: "easeInOut" }}
         className="flex flex-col gap-8 justify-center items-center"
